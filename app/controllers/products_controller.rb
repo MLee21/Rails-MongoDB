e3746ba94d56f6b3f4c925c2b5ed20e2d566d78b
MLee21 <minnie.lee89@gmail.com>
2015-07-27 13:44:48 -0600

    can update a product.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,10 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
   def show
     @product = Product.find(params[:id])
   end
@@ -20,6 +24,15 @@ class ProductsController < ApplicationController
     else
       flash[:error] = @product.errors.full_message.join(", ")
       render :new
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
     end
   end
 
--- spec/features/create_new_product_spec.rb
+++ spec/features/create_new_product_spec.rb
@@ -24,4 +24,18 @@ feature "a product" do
     expect(page).to have_content("Nexus 5")
     expect(page).to have_content(30000)
   end
+
+  scenario "can be edited" do 
+    visit products_path
+    find("#product_#{@product.id}").click_link 'Edit'
+    expect(current_path).to eq(edit_product_path(@product))
+    expect(page).to have_selector("input[value='Nexus 5']")
+    expect(page).to have_selector("input[value='30000']")    
+    fill_in 'product[price]', with: 25000
+    click_button 'Save Product'
+    expect(page).to have_content(25000)
+    expect(page).to have_content("Nexus 5")
+    visit products_path
+    expect(page).to_not have_content(30000)  
+  end
 end
