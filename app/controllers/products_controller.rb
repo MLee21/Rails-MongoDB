ece2e2676fe6d95673e59169adb9d4b55714a225
MLee21 <minnie.lee89@gmail.com>
2015-07-27 13:17:14 -0600

    deleted unnecessary tests that were generated automatically. able to create product.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,23 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = @product.errors.full_messages.join(", ")
+      redirect_to products_path
+    else
+      flash[:error] = @product.errors.full_message.join(", ")
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price)
   end
 end
--- spec/features/create_new_product_spec.rb
+++ spec/features/create_new_product_spec.rb
@@ -5,8 +5,8 @@ feature "a product" do
     visit products_path
     click_button 'New Product'
     expect(current_path).to eq(new_product_path)
-    fill_in :Name, with: 'Nexus 5'
-    fill_in :Price, with: '30000'
+    fill_in 'product[name]', with: 'Nexus 5'
+    fill_in 'product[price]', with: '30000'
     click_button 'Save Product'
     expect(current_path).to eq(products_path)
   end
