d609e139dff73a8c90763b14b6ae25c72c392496
MLee21 <minnie.lee89@gmail.com>
2015-07-27 11:36:53 -0600

    wrote test. created routes. setup controller and model. setup mongo

--- spec/requests/product_catalogs_spec.rb
+++ spec/requests/product_catalogs_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "ProductCatalogs", type: :request do
   describe "GET /product_catalogs" do
-    it "works! (now write some real specs)" do
-      get product_catalogs_path
-      expect(response).to have_http_status(200)
+    it "displays some products list" do
+      visit products_path
+      expect(page).to have_content("Nexus 5")
     end
   end
 end
